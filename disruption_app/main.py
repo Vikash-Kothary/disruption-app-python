25f104ea1af33b5814164309efcad78f9c3503a7
Vikash Kothary <kothary.vikash@gmail.com>
2018-11-24 21:33:59 +0000

    Rename main function for clarity

--- disruption_app/main.py
+++ disruption_app/main.py
@@ -11,8 +11,8 @@ db.create_tables()
 app.register_blueprint(views)
 
 
-def run():
+def main():
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == '__main__':
-    run()
+    main()
